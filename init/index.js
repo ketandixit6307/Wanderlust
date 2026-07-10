02bf11c2ccee6489b79c2d3d9a2031071c0c27a4
Ketan Dixit <ketandixit192@gmail.com>
2026-07-11 02:20:28 +0530

    Fix: Replace local MongoDB URL with Atlas URI in init/index.js

--- init/index.js
+++ init/index.js
@@ -1,18 +1,20 @@
 //node index.js
+require("dotenv").config({ path: require("path").join(__dirname, "../.env") });
 const mongoose  = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const ATLASDB_URI = process.env.ATLASDB_URI;
 
 main()
 .then (() =>{
-    console.log("connected to DB");
+    console.log("connected to Atlas DB");
 })
 .catch((err) =>{
     console.log(err);
 });
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(ATLASDB_URI);
 }
 
 const initDB = async () =>{
